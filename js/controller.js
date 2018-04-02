09cabfe1fa6257d5729ca5a13ef6a256bb10612e
yovchenko <yovchenko.w@gmail.com>
2018-04-03 01:15:11 +0300

    added csss

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,14 @@
 var app = angular.module('myApp', ['ngRoute'])
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
 	$routeProvider
 		.when('/Lviv', {
-			template : '<h1>Lviv</h1><p>is the largest city in western Ukraine and the seventh-largest city in the country overall, with a population of around 728,350 as of 2016. Lviv is one of the main cultural centres of Ukraine.</p>'
+			template: '<h1>Lviv</h1><p>is the largest city in western Ukraine and the seventh-largest city in the country overall, with a population of around 728,350 as of 2016. Lviv is one of the main cultural centres of Ukraine.</p><img src="photos/Lviv.jpg" alt="Lviv">'
 		})
 		.when('/Kharkiv', {
-			template : '<h1>Kharkiv</h1><p>is the second-largest city in Ukraine.The city was the first capital of the Ukrainian Soviet Socialist Republic, from December 1919 to January 1934.Population: 1, 439,036 (2017 est.)</p>'
+			template: '<h1>Kharkiv</h1><p>is the second-largest city in Ukraine.The city was the first capital of the Ukrainian Soviet Socialist Republic, from December 1919 to January 1934.Population: 1, 439,036 (2017 est.)</p><img src="photos/Kharkiv.jpg" alt="Kharkiv">'
 		})
 		.otherwise({
-			template : '<h1>Kyiv</h1><p>is the capital and largest city of Ukraine, located in the north central part of the country on the Dnieper.The population in July 2015 was 2,887,974</p>'
+			template: '<h1>Kyiv</h1><p>is the capital and largest city of Ukraine, located in the north central part of the country on the Dnieper.The population in July 2015 was 2,887,974</p><img src="photos/Kyiv.jpg" alt="Kyiv">'
 		})
 })
 app.controller('citiesCtrl', function ($scope) {
